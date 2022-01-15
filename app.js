72d2fcd390b77bea7ebe3d88c053ae6150ceb8e9
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-01-15 10:32:36 +0530

    refactor: upgraded to es6

--- app.js
+++ app.js
@@ -3,29 +3,24 @@ const submit = document.querySelector(".check")
 const process = document.querySelector(".timeout")
 const result = document.querySelector(".result")
 
-submit.addEventListener("click", checkPalindrome)
-
-
-function checkPalindrome() {
-    let bday = date.value;
+const checkPalindrome =() => {
+    const bday = date.value;
 
     if (date.value) {
         showProcess();
-
         setTimeout(() => {
             hideProcess();
         }, 1000);
 
-        let dateArr = bday.split("-");
-        let dateObj = {
+        const dateArr = bday.split("-");
+        const dateObj = {
             day: Number(dateArr[2]),
             month: Number(dateArr[1]),
             year: Number(dateArr[0]),
         }
 
-        let dateAsStr = getDateAsString(dateObj);
-        let list = checkPalindromeForAllDateFormats(dateAsStr);
-        console.log(list);
+        const dateAsStr = getDateAsString(dateObj);
+        const list = checkPalindromeForAllDateFormats(dateAsStr);
         let isPalindrome = false;
 
         list.forEach(item => {
@@ -41,29 +36,25 @@ function checkPalindrome() {
             result.innerText ="Nope! Your birthday is not a palindrome."
         }      
 
-
     } else {
-
         result.innerText = "Invalid Date";
-        
     }
     
 }
 
-function showProcess() {
+const showProcess = () => {
     process.style.display = "block";
     result.style.display = "none";
-
 }
 
-function hideProcess() {
+const hideProcess =()=> {
     process.style.display = "none";
     result.style.display = "block";
     
 }
 
-function getDateAsString(date) {
-    let dateAsStr = { day:"", month:"", year: date.year.toString()};
+const getDateAsString = date => {
+    const dateAsStr = { day:"", month:"", year: date.year.toString()};
 
     if (date.day <10) {
         dateAsStr.day = "0" + date.day;
@@ -79,12 +70,12 @@ function getDateAsString(date) {
     return dateAsStr;
 }
 
-function checkPalindromeForAllDateFormats(date) {   
-    var dateInAllFormats = getDateInAllFormats(date);
-    var palindromeList = [];
+const checkPalindromeForAllDateFormats = date => {   
+    const dateInAllFormats = getDateInAllFormats(date);
+    const palindromeList = [];
 
     dateInAllFormats.forEach(item => {
-        var ans =  isStringPalindrome(item);
+        const ans =  isStringPalindrome(item);
         palindromeList.push(ans);
     });
 
@@ -92,19 +83,21 @@ function checkPalindromeForAllDateFormats(date) {
     
 }
 
-function getDateInAllFormats(date) {
-    var ddmmyyyy = date.day + date.month + date.year;
-    var mmddyyyy = date.month + date.day + date.year;
-    var yyyymmdd = date.year + date.month + date.day;
-    var ddmmyy = date.day + date.month + date.year.slice(-2);
-    var mmddyy = date.month + date.day + date.year.slice(-2);
-    var yyddmm = date.year.slice(-2) + date.day + date.month;
+const getDateInAllFormats = date=> {
+    const ddmmyyyy = date.day + date.month + date.year;
+    const mmddyyyy = date.month + date.day + date.year;
+    const yyyymmdd = date.year + date.month + date.day;
+    const ddmmyy = date.day + date.month + date.year.slice(-2);
+    const mmddyy = date.month + date.day + date.year.slice(-2);
+    const yyddmm = date.year.slice(-2) + date.day + date.month;
   
     return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yyddmm];
 }
 
 
-function isStringPalindrome(item) {
+const isStringPalindrome = item => {
     var reversedItem = item.split("").reverse().join("");
     return item === reversedItem;
 }
+
+submit.addEventListener("click", checkPalindrome)
